ffcc20e6d5f685d43e71944551fd2a4f9d04899f
Wanderlei <devwanderlei@gmail.com>
2025-01-05 12:22:29 -0300

    removendo as configurações duplicadas do markdown

--- nvim/.config/nvim/lua/options.lua
+++ nvim/.config/nvim/lua/options.lua
@@ -15,22 +15,3 @@ autocmd("BufWritePre", {
 		vim.lsp.buf.format({ async = false })
 	end,
 })
-
--- Configurações específicas para Markdown
-vim.g.vim_markdown_folding_disabled = 1
-vim.g.vim_markdown_conceal = 0
-vim.g.vim_markdown_frontmatter = 1
-vim.g.vim_markdown_strikethrough = 1
-vim.g.vim_markdown_math = 1
-vim.g.vim_markdown_checkboxes = 1
-
--- Autocomandos para Markdown
-vim.cmd([[
-  augroup Markdown
-    autocmd!
-    autocmd FileType markdown setlocal wrap
-    autocmd FileType markdown setlocal spell
-    autocmd FileType markdown setlocal textwidth=80
-    autocmd FileType markdown setlocal conceallevel=0
-  augroup END
-]])
